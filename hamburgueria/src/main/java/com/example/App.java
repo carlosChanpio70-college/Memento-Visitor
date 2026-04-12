f3886695c09f1b8c88063767c2315ebcc67b49fd
Carlos Alexandre <carlosalexterra@gmail.com>
2026-04-12 18:26:15 -0300

    Initial commit

---
+++ hamburgueria/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Make a program for a "hamburgueria" that uses a decorator at least
+     */
+}
